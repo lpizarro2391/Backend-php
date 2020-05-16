caf16c3534bdc1ce2a46fa35dfd86f94cebf70c1
lpizarro <lpizarro2391@github.com>
2020-05-16 15:42:59 -0600

    indica que tipo de datos son

--- index.php
+++ index.php
@@ -7,5 +7,24 @@ function saludar(){
     global $persona1;
     echo "hola"." " .$persona1;
 }
-saludar()
+
+
+
+$a = true;
+$b = 2;
+$c = 2.76;
+$d = 'a';
+$e = [3,5,6];
+$f = new stdClass();
+$g = NULL;
+
+echo " ".gettype($a);
+echo " ".gettype($b);
+echo " ".gettype($c);
+echo " ".gettype($d);
+echo " ".gettype($e);
+echo " ".gettype($f);
+echo " ".gettype($g);
+
 ?>
+
