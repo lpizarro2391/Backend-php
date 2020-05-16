dfd03bb8eb208b7aedc9d0ec918140f09863c928
lpizarro <lpizarro2391@github.com>
2020-05-16 16:23:35 -0600

    en caso de ser verdadera la condicion se cierra el bloque php

---
+++ condicionales.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="uth-8">
+<title>Prueba condicionales</title>
+</head>
+<body>
+    <?php
+        $a = 2;
+        $b = 4;
+        $c = 6;
+
+        if($a < $b){
+            ?<
+
+
+
+        }
+    ?<
+
+
+
+</body>
+</html>
