4740ffed2415c08d0e1b066347c4d342669faee2
lpizarro <lpizarro2391@github.com>
2020-05-16 18:06:35 -0600

    do while, iteraciones termina con una condicion

--- ciclos.php
+++ ciclos.php
@@ -5,4 +5,10 @@ while($a <= 10){
     echo "<p> El valor de a es : ".$a."</p>";
     $a++;
 }
+echo "-------------------";
+$b = 0;
+do{
+    echo"<p> El valor de b es: ".$b."</p>";
+    $b++;
+} while ($b<=5);
 ?>
