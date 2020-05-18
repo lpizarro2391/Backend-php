b1ed78530c24f41750cfbfb2ff882294593d280f
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-18 15:17:14 -0600

    $this, constructor

--- clases.php
+++ clases.php
@@ -10,13 +10,19 @@
 
     const RUEDAS = 4;
 
+    public __construct($cilindraje, $modelo, $peso){
+        $this ->$cilindraje = $cilindraje;
+        $this->$modelo = $modelo;
+        $this->$peso = $peso;
+    }
+
     public function arrancar($velocidad){
         echo "<p>el auto ha arrancado a una velocidad de: ".$velocidad."km/h</p>";
 
     }
  }       
 
- $a = new Auto();
+ $a = new Auto(1300,'2010',215);
 
  echo $a->arrancar(20);
   
