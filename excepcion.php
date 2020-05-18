dcdd38520c57b55b43940b6d8e25e2ff8c98cb95
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-18 16:59:52 -0600

    Excepcion, si se divide entre cero, se ejecuta la excepción personalizada con trow new

---
+++ excepcion.php
@@ -0,0 +1,19 @@
+<?php
+
+function dividir($num1,$num2){
+if ($num2 == 0){
+    throw new Exception("No puede hacer una division entre cero");
+}
+
+return $num1/$num2;
+
+}
+
+try{
+    echo dividir(6,2);
+
+}catch(Exception $e){
+    echo "Se presentó un Error ".$e->getMessage();
+}
+
+?>
