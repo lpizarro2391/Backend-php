36bab6e8fa44a8b15e85f8a3202c4861d72f5900
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-18 15:42:26 -0600

    funciones operaciones aritmeticas básicas

---
+++ inclusion/operaciones.php
@@ -0,0 +1,27 @@
+<?php
+function sumar($n1,$n2){
+    return $n1+$n2;
+}
+
+function restar($n1, $n2)
+{
+    return $n1 - $n2;
+}
+
+function multiplicar($n1, $n2)
+{
+    return $n1 * $n2;
+}
+
+function dividir($n1, $n2)
+{
+    return $n1 / $n2;
+}
+
+function elevar($n1, $n2)
+{
+    return $n1 ** $n2;
+}
+
+
+?>
