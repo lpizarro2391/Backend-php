142b2c8a026b325e198d7c2203fbc14b448e812a
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-20 11:41:44 -0600

    Petición Ajax , mediante definimos el archivo php que va a recibir la información mediante el atributo URL

--- C6_U3_L4 Manejo_formularios/js/index.js
+++ C6_U3_L4 Manejo_formularios/js/index.js
@@ -9,6 +9,12 @@ $(function(){
   $('formulario').submit(function(event){
     var nombre= $('form').find('input[name = "nombre_usuario"]').val();
     event.preventDefault();
+    $.ajax(
+        {
+        url: './recepcion_formulario.php',
+        }
+
+    )
 
 
 
