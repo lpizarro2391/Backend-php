0f0f08982649d6fff358f9048a312a8daca0b8e5
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 14:58:05 -0600

    creamos variable exito para corroborar si ocurrio algun problema en las verificaciones anteriores, en caso de ser verdadero , creamos un nuevo condicional, en cual ejecutarmos move, que mueve un archivo temporal nuevo al un servidor , usando tp-name,  si ésta operacion retorna true, indica que fué exito, de lo contrario se envía el mensaje de error.

--- addimage.php
+++ addimage.php
@@ -5,22 +5,35 @@ $archivo_a_subir = $directorio.basename($nombreImagen);
 $tipoArchivo = pathinfo($archivo_a_subir,PATHINFO_EXTENSION);
 $taamano_arhivo = S_FILES["file"]["size"];
 $exito = true;
-$respuesta;
+$respuestas;
 
 if (file_exists($archivo_a_subir)){
-$respuesta["mensaje"] = "El archivo ya existe: ";
+$respuestas["mensaje"] = "El archivo ya existe: ";
 $exito = false;
 
 }
 
 if ($taamano_arhivo > 2000000) {
-    $respuesta["mensaje"] = "El archivo es demasiado grande";
+    $respuestas["mensaje"] = "El archivo es demasiado grande";
     $exito = false;
 
 if ($tipoArchivo !="jpg" && tipoArchivo != "png" && tipoArchivo != "jpeg" && tipoArchivo != "gif"){
     respuestas["mensaje"] = "Solo se permiten archivos JPG,JPEG,PNG & GIF";
     $exito= false;
 }
+
+if($exito == false) {
+    $respuestas["mensaje"] = "Lo sentimos , tu archivo no fué añadido";
+}else{
+    if(move_uploaded_file($_FILES["file"]["tmp_name"], $archivo_a_subir)){
+        $respuestas["final"] = "El archivo ".basename($nombreImagen)."Ha sido añadido";
+        $respuestas["mensaje"] = "";
+        $respuestas["newSource"] = $archivo_a_subir;
+    }else{
+        $respuestas["final"] = "Lo sentimos, tu archivo no fué añadido";
+    }
+}
+
 
 }
 
