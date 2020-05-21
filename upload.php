37edceb74ebaf28300db5c4dfcf4d51c3a38f3e1
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 12:13:11 -0600

    asignamos a la posucion del arrreglo response el titulo del archivo leido

--- upload.php
+++ upload.php
@@ -2,5 +2,5 @@
 $titulo = $_POST("titulo");
 $file = fopen(".uploaded-files/".$titulo,"r");
 $response["contenido"] = fread($file,filesize(".uploaded-files/".$titulo));
-
+$response["titulo"] = $titulo;
 ?>
