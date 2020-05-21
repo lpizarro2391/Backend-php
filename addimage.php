b436fb3466d3a6727c1b1378185d05ca854042cc
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 14:28:36 -0600

    tamaño del archivo

--- addimage.php
+++ addimage.php
@@ -3,8 +3,11 @@ $directorio = "uploads/";
 $nombreImagen = $_FILES["file"]["name"];
 $archivo_a_subir = $directorio.basename($nombreImagen);
 $tipoArchivo = pathinfo($archivo_a_subir,PATHINFO_EXTENSION);
+$taamanoarhivo = S_FILES["file"]["size"];
 $exito = true;
 $respuesta;
+
+
 
 
 
