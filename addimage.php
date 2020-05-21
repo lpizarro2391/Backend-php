e954164500f653b5a476e1567e732d7e6ee372c1
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 14:45:12 -0600

    3 condicionales para verificar algunos aspectos sobre el archivo a subir , 1. comprobar que el archuvo ya no se haya subido antes para evitar duplicados, file_exist 2. verificar el tamaño del archivo que no sea muy grande 3. verificar que el archivo subido sea mediante las exetenciones permitidas , por ultimo se usa la variable exito para verirficar si hay alguna problema en las verificcaiones anteriores

--- addimage.php
+++ addimage.php
@@ -3,10 +3,26 @@ $directorio = "uploads/";
 $nombreImagen = $_FILES["file"]["name"];
 $archivo_a_subir = $directorio.basename($nombreImagen);
 $tipoArchivo = pathinfo($archivo_a_subir,PATHINFO_EXTENSION);
-$taamanoarhivo = S_FILES["file"]["size"];
+$taamano_arhivo = S_FILES["file"]["size"];
 $exito = true;
 $respuesta;
 
+if (file_exists($archivo_a_subir)){
+$respuesta["mensaje"] = "El archivo ya existe: ";
+$exito = false;
+
+}
+
+if ($taamano_arhivo > 2000000) {
+    $respuesta["mensaje"] = "El archivo es demasiado grande";
+    $exito = false;
+
+if ($tipoArchivo !="jpg" && tipoArchivo != "png" && tipoArchivo != "jpeg" && tipoArchivo != "gif"){
+    respuestas["mensaje"] = "Solo se permiten archivos JPG,JPEG,PNG & GIF";
+    $exito= false;
+}
+
+}
 
 
 
