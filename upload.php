de1211bea3ebd55989009ec88870082b0eef37ae
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 12:15:40 -0600

    Cerrar archivos , despues de abrirlos,  al terminar de procesarlos con la funcion fclose, el único parametro que recibe es el archivo aberito en el script, éseto para evitar dejar archivos aviertos sin necesitdad cosumiendo recursos. Añadimos una sentencia para retornar a la vista el arreglo asociativo en formato json.

--- upload.php
+++ upload.php
@@ -3,4 +3,5 @@ $titulo = $_POST("titulo");
 $file = fopen(".uploaded-files/".$titulo,"r");
 $response["contenido"] = fread($file,filesize(".uploaded-files/".$titulo));
 $response["titulo"] = $titulo;
+echo json_encode($response);
 ?>
