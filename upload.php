25bc8faa9783c47ee20066866ea3af53c1051edc
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 12:16:39 -0600

    fclose, sobre el archivo abierto,

--- upload.php
+++ upload.php
@@ -4,4 +4,5 @@ $file = fopen(".uploaded-files/".$titulo,"r");
 $response["contenido"] = fread($file,filesize(".uploaded-files/".$titulo));
 $response["titulo"] = $titulo;
 echo json_encode($response);
+fclose($file);
 ?>
