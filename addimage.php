0d54e9f3cc7de9a4e7daefca78ba5b73a8c310f5
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 14:25:10 -0600

    obtener el nombre en cadena de caracteres

--- addimage.php
+++ addimage.php
@@ -2,4 +2,8 @@
 $directorio = "uploads/";
 $nombreImagen = $_FILES["file"]["name"];
 $archivo_a_subir = $directorio.basename($nombreImagen);
+$tipoArchivo = pathinfo($archivo_a_subir,PATHINFO_EXTENSION);
+
+
+
 ?>
