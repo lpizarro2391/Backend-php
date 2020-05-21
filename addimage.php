4d830d5936a48c68466243e5b49bf9bd7e937c5c
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 14:26:50 -0600

    almacenamos en una variable el tamano del archivo usando la palabra size en el arreglo asociativo y usamos 2 variables de referencia para enviar la respuesta a la vista

--- addimage.php
+++ addimage.php
@@ -3,6 +3,11 @@ $directorio = "uploads/";
 $nombreImagen = $_FILES["file"]["name"];
 $archivo_a_subir = $directorio.basename($nombreImagen);
 $tipoArchivo = pathinfo($archivo_a_subir,PATHINFO_EXTENSION);
+$exito = true;
+$respuesta;
+
+
+
 
 
 
