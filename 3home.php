b83df48e4d796c0cac69c7bc3962674f722fd05c
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 16:05:11 -0600

    de lo contrario almacenamos true y los datos de variables almacenados en los datos de sesion creadas

--- 3home.php
+++ 3home.php
@@ -2,5 +2,10 @@
 session_start();
 if (!isset($_SESSION['nombre'])){
     $response['msj'] = "false";
+}  else{
+    $response['msj'] = "true";
+    $response['nombre']= $_SESSION['nombre'];
+    $response['img'] = $_SESSION['img-profile'];
 }
+echo json_encode($response);
 ?>
