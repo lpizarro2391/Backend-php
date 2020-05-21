02806fe6d14945c88a22c8f43e238e4b476646af
lpizarro2391 <51811145+lpizarro2391@users.noreply.github.com>
2020-05-21 12:26:59 -0600

    fwrite para indicar el archivo abierto en la variable new file,  y luego la variable texto en la que se encuentra el contenido proveiente  de la vista, cerramos el archivo y enviaos la respuesat a la vista ,

--- addfile.php
+++ addfile.php
@@ -2,7 +2,8 @@
 $titulo = $_POST('titulo');
 $texto = $_POST('texto');
 $newfile = fopen('./uploaded-files'.$titulo,"w") or die ("Error de archivo");
-
+fwrite($newfile,$texto);
+fclose($newfile);
 
 
 
